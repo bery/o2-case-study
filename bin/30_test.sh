597cacf4d5abdb909d459f8b85afe4b0b8d3d0f2
Lukas Beranek <lukas@beecom.io>
2020-08-03 20:57:43 +0200

    resources

--- bin/30_test.sh
+++ bin/30_test.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # Proxy the desired service to local port using port forwarding
 set -x
-
 APP_LABEL=${APP_LABEL:-"appa"}
+
 export POD_NAME=$(kubectl get pods --namespace default \
   -l "app.kubernetes.io/name=app,app.kubernetes.io/instance=${APP_LABEL}" -o jsonpath="{.items[0].metadata.name}") && \
 echo "Visit http://127.0.0.1:8080 to use your application" && \
-kubectl port-forward $POD_NAME 8080:80
+kubectl port-forward "$POD_NAME" 8080:80
